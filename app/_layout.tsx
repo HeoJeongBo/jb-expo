35d6e3bfcc35d4e3d19b89583d17216abe75843a
heojeongbo <heojungbo@gmail.com>
2024-11-20 20:44:17 +0900

    feat: add default fsd structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
+
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -26,7 +28,6 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
     </>
   );
 }
